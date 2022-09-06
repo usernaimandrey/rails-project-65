e01a49bb61f66f040ccd9640d44629ed4c318712
usernaimandrey <anshlyapnikov@yandex.ru>
2022-09-06 19:03:48 +0300

    fixed_actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_06_115129) do
+ActiveRecord::Schema.define(version: 2022_08_30_084312) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
